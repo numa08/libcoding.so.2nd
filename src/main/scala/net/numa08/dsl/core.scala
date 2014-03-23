bbba65213711cb05807785453b785333b2dba313
numa08 <n511287@gmail.com>
2014-03-23 11:59:04 +0900

    dslできた

---
+++ src/main/scala/net/numa08/dsl/core.scala
@@ -0,0 +1,32 @@
+package net.numa08.dsl
+
+import scala.collection.mutable.ArrayBuffer
+
+
+class Performer(name : String) {
+	val cs = ArrayBuffer[String]()
+
+	def -(content: String) : Performer = {
+		cs += content
+		this
+	}
+
+	override def toString = {
+		name + "\n" + cs.mkString("\t-", "\n", "\n")
+	}
+}
+
+trait Performers {
+	val ps = ArrayBuffer[Performer]()
+
+	def add(name : String) : Performer = {
+		val p = new Performer(name)
+		ps += p
+		p
+	}
+
+	implicit def string2performer(name : String) : Performer = {
+		val p = add(name)
+		p
+	}
+}
--- src/main/scala/net/numa08/libcoding_so2/Libcoding_so_2.scala
+++ src/main/scala/net/numa08/libcoding_so2/Libcoding_so_2.scala
@@ -1,5 +1,17 @@
 package net.numa08.libcoding_so2
 
-object Libcoding_so_2 extends App {
-  println("Hello, Libcoding_so_2")
+import net.numa08.dsl._
+
+object Libcoding_so_2 extends App with Performers{
+
+	"ねおさん".
+		-("なんかやる")
+
+	"mironalさん".
+		-("なんかやる")
+
+	"けいまさん".
+		-("なんかやる")
+	
+	ps.foreach(println(_))
 }
